79839c0310a3b935e1f575ea62f1472904ab8f4c
George Higbie <gehigbie@gmail.com>
2017-02-28 15:34:33 -0700

    identified recycler view in java

--- app/src/main/java/com/geogehigbie/slopesradio/fragment/StationsFragment.java
+++ app/src/main/java/com/geogehigbie/slopesradio/fragment/StationsFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,8 +65,12 @@ public class StationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stations, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view1)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stations, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
