c5f15c56e8bf35483ddb59401ea0399aced44118
George Higbie <gehigbie@gmail.com>
2017-02-28 14:42:44 -0700

    initial commit

---
+++ app/src/main/java/com/geogehigbie/slopesradio/activities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.geogehigbie.slopesradio.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.geogehigbie.slopesradio.R;
+import com.geogehigbie.slopesradio.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MainFragment mainFragment = (MainFragment) fragmentManager.findFragmentById(R.id.container_main);
+
+        if(mainFragment == null){
+            mainFragment = MainFragment.newInstance("blah", "kah");
+            fragmentManager.beginTransaction().add(R.id.container_main, mainFragment).commit();
+        }
+    }
+}
