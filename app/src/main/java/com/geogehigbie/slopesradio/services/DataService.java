852e91e0d0af93407d71a7cf8821139c4f76da6b
George Higbie <gehigbie@gmail.com>
2017-03-01 15:54:06 -0700

    added method to return list

--- app/src/main/java/com/geogehigbie/slopesradio/services/DataService.java
+++ app/src/main/java/com/geogehigbie/slopesradio/services/DataService.java
@@ -1,5 +1,9 @@
 package com.geogehigbie.slopesradio.services;
 
+import com.geogehigbie.slopesradio.model.Station;
+
+import java.util.ArrayList;
+
 /**
  * Created by georgehigbie on 3/1/17.
  */
@@ -12,4 +16,29 @@ public class DataService {
 
     private DataService() {
     }
+
+    public ArrayList <Station> getFeaturedStations(){
+        //pretend we just downloaded featured stations from internet
+
+        ArrayList<Station> list = new ArrayList<>();
+        list.add(new Station("Flight Plan (Tunes for Travel)", "flightPlanMusic"));
+        list.add(new Station("Two-Wheeling (Biking Play List)", "bicycleMusic"));
+        list.add(new Station("Kids Jams (Music for Children)", "kidsmusic"));
+
+        return list;
+    }
+
+//    public ArrayList <Station> getRecentStations(){
+//        ArrayList<Station> list = new ArrayList<>();
+//        list.add();
+//
+//        return list;
+//    }
+//
+//    public ArrayList <Station> getPartyStations(){
+//        ArrayList<Station> list = new ArrayList();
+//        list.add();
+//
+//        return list;
+//    }
 }
